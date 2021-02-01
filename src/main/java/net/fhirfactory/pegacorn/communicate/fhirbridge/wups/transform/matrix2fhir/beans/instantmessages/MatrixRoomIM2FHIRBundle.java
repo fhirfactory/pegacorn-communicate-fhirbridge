1268cd7a8fd719abd66eaf6ade2a8dad7557dbbf
Mark Hunter <m.a.hunter@outlook.com>
2021-02-01 16:02:42 +1100

    Working Version with Single Mapping Function

---
+++ src/main/java/net/fhirfactory/pegacorn/communicate/fhirbridge/wups/transform/matrix2fhir/beans/instantmessages/MatrixRoomIM2FHIRBundle.java
@@ -0,0 +1,147 @@
+package net.fhirfactory.pegacorn.communicate.fhirbridge.wups.transform.matrix2fhir.beans.instantmessages;
+
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.parser.IParser;
+import net.fhirfactory.pegacorn.communicate.fhirbridge.core.common.exceptions.MajorTransformationException;
+import net.fhirfactory.pegacorn.communicate.fhirbridge.core.common.exceptions.MatrixMessageException;
+import net.fhirfactory.pegacorn.datasets.fhir.r4.base.entities.bundle.MessageHeaderHelper;
+import net.fhirfactory.pegacorn.deployment.names.PegacornCommunicateComponentNames;
+import net.fhirfactory.pegacorn.deployment.names.PegacornLadonComponentNames;
+import org.hl7.fhir.r4.model.Bundle;
+import org.hl7.fhir.r4.model.Communication;
+import org.hl7.fhir.r4.model.MessageHeader;
+import org.json.JSONException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@ApplicationScoped
+public class MatrixRoomIM2FHIRBundle {
+    private static final Logger LOG = LoggerFactory.getLogger(MatrixRoomIM2FHIRBundle.class);
+
+    @Inject
+    private MatrixRoomIM2FHIRCommunication im2FHIRCommunication;
+
+    @Inject
+    private MessageHeaderHelper messageHeaderHelper;
+
+    @Inject
+    private PegacornCommunicateComponentNames pegacornCommunicateComponentNames;
+
+    @Inject
+    private PegacornLadonComponentNames pegacornLadonComponentNames;
+
+    /**
+     *
+     * This function takes an incoming Matrix Room Instant Message
+     * (Matrix::m.room.message) and converts it to a FHIR::Bundle containing a
+     * FHIR::MessageHeader and a FHIR::Communication element set.
+     * <p>
+     * Where the message is originating from a Matrix::User, the function
+     * looks-up the PractitionerID2MatrixName Cachemap to ascertain if there is
+     * any pre-existing mapping between a Matrix User and a FHIR::Practitioner.
+     * If not, it creates a temporary FHIR::Identifier (Use = TEMP). This
+     * FHIR::Identifier is then used to construct the FHIR::Reference for the
+     * FHIR::Communication.Sender field.
+     * <p>
+     * Similarly, depending on the Matrix::Room (Group) that the message is
+     * associated with, the FHIR::Communication.Subject field is populated with
+     * a FHIR::Reference associated with a FHIR::CareTeam, FHIR::Group,
+     * FHIR::Organisation, FHIR::PractitionerRole or FHIR::Patient.
+     * <p>
+     * If the message (Matrix::m.room.message) is part of a peer-to-peer
+     * dialogue, then the FHIR::Communication.Target is populated with the
+     * appropriate FHIR::Practitioner or FHIR::PractitionerRole. Again, the
+     * FHIR::Identifier for these may be queried from the
+     * PractitionerID2MatrixName map.
+     *
+     * @param roomInstantMessage The incoming Matrix Room Instant Message
+     * @return A List of FHIR::Bundle element, each comprising -->
+     * FHIR::MessageHeader, FHIR::Communication
+     * @throws MatrixMessageException
+     * @throws JSONException
+     * @throws MajorTransformationException
+     *
+     * @see
+     * <a href="https://matrix.org/docs/spec/client_server/r0.6.0#room-event-fields">Matrix Client-Server API Specificaton, Release 0.6.0 - "room_instant_message" Message</a> <p>
+     * <a href="https://www.hl7.org/fhir/bundle.html">FHIR Specification, Release 4.0.1, "Bundle" Resource</a>
+     *
+     */
+    public List<Bundle> convertMatrixInstantMessage2FHIRElements(String roomInstantMessage)
+            throws MatrixMessageException, JSONException, MajorTransformationException
+    {
+        LOG.debug("convertMatrixInstantMessage2FHIRElements(): Entry, Matrix Room Instant Message --> {}", roomInstantMessage);
+        if (roomInstantMessage == null) {
+            throw (new MatrixMessageException("Matrix Room Instant Message --> is null"));
+        }
+        if (roomInstantMessage.isEmpty()) {
+            throw (new MatrixMessageException("Matrix Room Instant Message --> is empty"));
+        }
+        // We need some helper functions to encode/decode the FHIR based message structures
+        LOG.trace("wrapCommunicationBundle(): Initialising FHIR Resource Parser & Setting Pretty Print");
+        FhirContext fhirContextHandle = FhirContext.forR4();
+        IParser fhirResourceParser = fhirContextHandle.newJsonParser();
+        // Now set up Iterator on the Ingres Content
+        Communication newCommunication = im2FHIRCommunication.buildCommunicationResourceFromMatrixIMMessage(roomInstantMessage);
+        MessageHeader newMessageHeader = messageHeaderHelper.buildMessageHeader(pegacornCommunicateComponentNames.getPegacornCommunicateFHIRBridgeSubsystem(), pegacornLadonComponentNames.getLadonSubsystemDefault());
+        Bundle newCommunicationBundle = buildCommunicationBundle(newMessageHeader, newCommunication);
+        ArrayList<Bundle> newOutputSet = new ArrayList<>();
+        newOutputSet.add(newCommunicationBundle);
+        return (newOutputSet);
+    }
+
+    /**
+     *
+     * This function wraps FHIR::MessageHeader and FHIR::Communication element
+     * set into a FHIR::Bundle.
+     * <p>
+     * Note that The bundles are always of type POST for FHIR::Communication
+     * based FHIR::Bundles.
+     *
+     * @param newMessageHeader The FHIR::MessageHeader associated with the
+     * FHIR::Communication element
+     * @param newCommunication The FHIR::Communication message generated as part
+     * of the transformation
+     * @return Bundle: A singular FHIR::Bundle made up of the
+     * FHIR::MessageHeader and FHIR::Communication elements.
+     */
+    private Bundle buildCommunicationBundle(MessageHeader newMessageHeader, Communication newCommunication)
+            throws MatrixMessageException, JSONException, MajorTransformationException
+    {
+        LOG.debug("buildCommunicationBundle(): Entry, FHIR::MessageHeader --> {}, FHIR::Communication --> {}", newMessageHeader, newCommunication);
+
+        // Before we do anything, let's confirm the (superficial) integrity of the incoming objects
+        LOG.trace("buildCommunicationBundle(): Checking integrity of the incoming Matrix Instant Message");
+        if (newMessageHeader == null) {
+            LOG.error("buildCommunicationBundle: MessageHeader resource is null!!!");
+            throw (new MajorTransformationException("wrapCommunicationBundle: FHIR::MessageHeader resource is null"));
+        }
+        if (newCommunication == null) {
+            LOG.error("buildCommunicationBundle: Communication resource is null!!!");
+            throw (new MajorTransformationException("wrapCommunicationBundle: FHIR::Communication resource is null"));
+        }
+        LOG.trace("buildCommunicationBundle(): Creating FHIR::Bundle and setting the FHIR::Bundle.BundleType");
+        Bundle newBundleElement = new Bundle();
+        newBundleElement.setType(Bundle.BundleType.MESSAGE);
+        LOG.trace("buildCommunicationBundle(): Creating FHIR::BundleEntryComponent for FHIR::MessageHeader resource");
+        Bundle.BundleEntryComponent bundleEntryForMessageHeaderElement = new Bundle.BundleEntryComponent();
+        bundleEntryForMessageHeaderElement.setResource(newMessageHeader);
+        LOG.trace("buildCommunicationBundle(): Creating FHIR::BundleEntryComponent for FHIR::Communication resource");
+        Bundle.BundleEntryComponent bundleEntryForCommunicationElement = new Bundle.BundleEntryComponent();
+        bundleEntryForCommunicationElement.setResource(newCommunication);
+        LOG.trace("buildCommunicationBundle(): Creating Adding the MessageHeader BundleEntryComponent & Communication BundleEntryComponent to the Bundle resource");
+        newBundleElement.addEntry(bundleEntryForMessageHeaderElement);
+        newBundleElement.addEntry(bundleEntryForCommunicationElement);
+        LOG.trace("buildCommunicationBundle(): Setting the message/bundle creation date");
+        newBundleElement.setTimestamp(new Date());
+        LOG.trace("buildCommunicationBundle(): Setting the message/bundle resource count - 2 in this case (i.e. MessageHeader + Communication resources");
+        newBundleElement.setTotal(2);
+        LOG.debug("wrapCommunicationBundle(): Exit, Created FHIR::Bundle --> {}", newBundleElement);
+        return (newBundleElement);
+    }
+}
